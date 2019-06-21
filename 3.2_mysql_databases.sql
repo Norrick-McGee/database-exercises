2ea775ba9cc85e36907e094da7c54a436e451af1
Norrick <Norrick.McGee@gmail.com>
2019-06-21 14:42:27 -0500

    First 3 Intro SQL Scripts written

---
+++ 3.2_mysql_databases.sql
@@ -0,0 +1,5 @@
+SHOW DATABASES;
+USE ada_671;
+SELECT database();
+USE iris_db;
+SELECT database();
---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,11 @@
+SHOW DATABASES;
+USE employees;
+SHOW TABLES;
+DESCRIBE employees;
+#Primary Key emp_no
+#Describers: gender, hire_date, birth_date
+#Identifiers: first_name, last_name, gender, birth_date
+DESCRIBE departments;
+#Primary Key dept_no
+#Unique Keys dept_name
+SHOW CREATE TABLE departments;
